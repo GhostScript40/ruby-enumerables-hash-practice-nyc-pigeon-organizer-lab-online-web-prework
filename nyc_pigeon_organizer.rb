b4bc15eac7ee705c8be72d096707f090d071b725
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-07 09:09:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,9 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
 pigeon = {}
-names = data[:gender][0].each do | key, value|
-  pigeon[value]
+names = data[:gender].each do | key, value|
+male = names[value][male].each
+female = names[value][female].each
 binding.pry
   end
 end
